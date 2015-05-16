a18f11b732188d33f67e403c8f81422bd047f9bd
Janus Nicon <janusnic@gmail.com>
2015-05-16 23:33:23 +0300

    Add login repo

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     
     url(r'^categories/(?P<categoryslug>.*)/$', 'blog.views.category'),
     url(r'^posts/(?P<postslug>.*)/$', 'blog.views.view'),
+    url(r'^login/$', 'blog.views.ulogin', name='login'),
+    url(r'^restricted/', 'blog.views.restricted', name='restricted'),
+    url(r'^logout/$', 'blog.views.user_logout', name='logout'),
     #url(r'^search-form/$', views.search_form),
     #url(r'^search/$', views.search),
 ]
